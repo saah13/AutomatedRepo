9f7eaac46ad7b81602d73c9e14457c5f62b64490
Anton Li <servername1@gmail.com>
2017-10-10 12:30:37 +0300

    added Comparison of numbers and switch usage

---
+++ ComparisonOfNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ComparisonOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int randomNumber;
+            Random RNG = new Random();
+            randomNumber = RNG.Next(1, 10);
+            Console.WriteLine("Enter the number");
+            int y = Convert.ToInt32(Console.ReadLine());
+            if (randomNumber == y)
+            {
+                Console.WriteLine("Congratulations");
+                Console.ReadKey();
+            }
+            else if(randomNumber >= y)
+            {
+                Console.WriteLine($"Your number is smaller,number was {randomNumber} ");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine($"Your number is bigger,number was {randomNumber}");
+                Console.ReadKey();
+            }
+           
+            
+        }
+    }
+}
---
+++ SwitchUsage/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SwitchUsage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1-Vanilla ice cream \n2-Coca cola \n3-Salad \n4-Water \n5-Pizza \n:Enter the number of item:");
+            string selection = Console.ReadLine();
+            switch (selection)
+            {
+                case "1":
+                    Console.WriteLine("You selected Vanilla ice cream");
+                    break;
+                case "2":
+                    Console.WriteLine("You selected Coca cola");
+                    break;
+                case "3":
+                    Console.WriteLine("You selected Salad");
+                    break;
+                case "4":
+                    Console.WriteLine("You selected Water");
+                    break;
+                case "5":
+                    Console.WriteLine("You selected Pizza");
+                    break;
+                default:
+                    Console.WriteLine("You selected wrong number");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
