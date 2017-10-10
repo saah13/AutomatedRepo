1b356bce4102a730acbaf1cb4709718e6b623d62
Anton Li <servername1@gmail.com>
2017-10-10 15:06:50 +0300

    ?:Operator usage

---
+++ UsageOfConditional/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UsageOfConditional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int res = x != 0 ? 10 / x : x;
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
