554a45cddbb0fe5b3ccea6b6b1a1f1ddfbe20b2b
Anton Li <servername1@gmail.com>
2017-10-20 01:27:42 +0300

    List + Dictionary

---
+++ IntBasedDictionary/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntBasedDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Word> dictionary = new List<Word>();
+            for (int i = 10; i > 0; i--)
+            {
+                string guid = Guid.NewGuid().ToString();
+                Console.WriteLine($"Enter {i} more");
+                dictionary.Add(new Word() { Content = Console.ReadLine(), Id = guid });
+            }
+            foreach (Word i in dictionary)
+            {
+                Console.WriteLine($"{i.Id}   {i.Content}");
+            }
+            Console.ReadKey();
+        }
+        public class Word
+        {
+            public string Id { get; set; }
+            public string Content { get; set; }
+        }
+    }
+
+}
---
+++ List/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x;
+            List<string> list = new List<string>();
+            do
+            {
+                Console.WriteLine("Enter some text");
+                list.Add(Console.ReadLine());
+                do
+                {
+                    Console.WriteLine("Continue [Y/N]");
+                    x = Console.ReadLine();
+
+                } while ((x != "Y" && x != "N"));
+
+            } while (x != "N");
+            foreach (string i in list)
+            {
+                Console.WriteLine(i);
+            }
+            do
+            {
+                Console.WriteLine("Wanna delete something [Y/N]");
+                x = Console.ReadLine();
+            } while ((x != "Y" && x != "N"));
+            if (x == "Y")
+            {
+                Console.WriteLine("Enter what you want to be deleted");
+                list.Remove(Console.ReadLine());
+                foreach (string i in list)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine("Congratulations");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Congratulations");
+                Console.ReadKey();
+            }
+        }
+    }
+}
