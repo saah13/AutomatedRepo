132c0aab393bc4fd07c0a8b25215511d0eaf27b6
Anton Li <servername1@gmail.com>
2018-02-11 16:04:24 +0200

    Folders

---
+++ FioSwither/FioSwitherM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FioSwither
+{
+    public static class FioSwitherM
+    {
+        public static void Split(string data)
+        {
+            string[] arr = data.Split(new char[] { ' ' });
+            Regex initials = new Regex(@"(\b[a-zA-Z])[a-zA-Z]* ?");
+            var first = arr.First();
+            var second = initials.Replace(arr.ElementAt(1),"$1");
+            var third = initials.Replace(arr.ElementAt(2), "$1");
+            Console.WriteLine($"{first} {second}.{third}.");
+        }
+    }
+}
---
+++ FioSwither/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FioSwither
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FioSwitherM.Split("Lisetskyi Anton Oleksandrovich");
+            Console.ReadKey();
+        }
+    }
+}
